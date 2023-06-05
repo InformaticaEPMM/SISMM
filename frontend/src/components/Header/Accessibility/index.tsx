5f7f880098f1fbde6b21c436eef0afee9fed7c4d
DouglasNeres <douglasneres06@gmail.com>
2023-06-05 15:40:30 -0300

    Indentation of all files in standard format with eslint and prettier

--- frontend/src/components/Header/Accessibility/index.tsx
+++ frontend/src/components/Header/Accessibility/index.tsx
@@ -1,54 +1,54 @@
-import { RiContrastFill } from 'react-icons/ri'
+import { useState, useEffect } from 'react';
+import { RiContrastFill } from 'react-icons/ri';
 
-import './styles.css'
-import { useState, useEffect } from 'react'
+import './styles.css';
 
 export function AccessibilityHeader() {
-	const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false)
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
 
-	// Verificar se há uma preferência de tema salva na localStorage
-	useEffect(() => {
-		const savedTheme = localStorage.getItem('theme')
-		if (savedTheme) {
-			setIsDarkTheme(savedTheme === 'dark')
-			document.querySelector('body')?.setAttribute('data-theme', savedTheme)
-		}
-	}, [])
+  // Verificar se há uma preferência de tema salva na localStorage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setIsDarkTheme(savedTheme === 'dark');
+      document.querySelector('body')?.setAttribute('data-theme', savedTheme);
+    }
+  }, []);
 
-	const setDarkMode = () => {
-		document.querySelector('body')?.setAttribute('data-theme', 'dark')
-		setIsDarkTheme(true)
-		localStorage.setItem('theme', 'dark') // Salvar a preferência de tema na localStorage
-	}
+  const setDarkMode = () => {
+    document.querySelector('body')?.setAttribute('data-theme', 'dark');
+    setIsDarkTheme(true);
+    localStorage.setItem('theme', 'dark'); // Salvar a preferência de tema na localStorage
+  };
 
-	const setLightMode = () => {
-		document.querySelector('body')?.setAttribute('data-theme', 'light')
-		setIsDarkTheme(false)
-		localStorage.setItem('theme', 'light') // Salvar a preferência de tema na localStorage
-	}
+  const setLightMode = () => {
+    document.querySelector('body')?.setAttribute('data-theme', 'light');
+    setIsDarkTheme(false);
+    localStorage.setItem('theme', 'light'); // Salvar a preferência de tema na localStorage
+  };
 
-	const toggleTheme = () => {
-		if (isDarkTheme) setLightMode()
-		else setDarkMode()
-	}
+  const toggleTheme = () => {
+    if (isDarkTheme) setLightMode();
+    else setDarkMode();
+  };
 
-	return (
-		<div className="bgAccessibilityField">
-			<div className="accessibilityField">
-				<div className="identity">
-					<span>EEEP MANOEL MANO - CRATEÚS</span>
-				</div>
+  return (
+    <div className="bgAccessibilityField">
+      <div className="accessibilityField">
+        <div className="identity">
+          <span>EEEP MANOEL MANO - CRATEÚS</span>
+        </div>
 
-				<div className="iconsAccessibility">
-					<span>A</span>
-					<span>A-</span>
-					<span>A+</span>
-					<RiContrastFill
-						style={{ transform: 'rotate(30deg)' }}
-						onClick={toggleTheme}
-					/>
-				</div>
-			</div>
-		</div>
-	)
+        <div className="iconsAccessibility">
+          <span>A</span>
+          <span>A-</span>
+          <span>A+</span>
+          <RiContrastFill
+            style={{ transform: 'rotate(30deg)' }}
+            onClick={toggleTheme}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,21 +1,20 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
 
-import './styles.css'
+import './styles.css';
 
 export function Main() {
-
-	return (
-		<div className="mainTop">
-			<div className="goalsMeeting">
-				<NavLink to="/">Metas </NavLink>
-				<NavLink to="/">Reuniões</NavLink>
-			</div>
-			<div className="subjects">
-				<Link to="">Linguagens e Códigos</Link>
-				<Link to="">Humanas</Link>
-				<Link to="">Natureza e Matemática</Link>
-				<Link to="">Base Técnica</Link>
-			</div>
-		</div>
-	)
+  return (
+    <div className="mainTop">
+      <div className="goalsMeeting">
+        <NavLink to="/">Metas </NavLink>
+        <NavLink to="/">Reuniões</NavLink>
+      </div>
+      <div className="subjects">
+        <Link to="">Linguagens e Códigos</Link>
+        <Link to="">Humanas</Link>
+        <Link to="">Natureza e Matemática</Link>
+        <Link to="">Base Técnica</Link>
+      </div>
+    </div>
+  );
 }
